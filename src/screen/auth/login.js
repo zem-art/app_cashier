9ff1ef8d0b905f5fdd299c3463e828ce7a889361
shin-wolf <amonsefra13@gmail.com>
2021-02-09 22:07:59 +0700

    Create Login With Api

--- src/screen/auth/login.js
+++ src/screen/auth/login.js
@@ -7,9 +7,13 @@ import {
   ScrollView,
   TextInput,
   TouchableOpacity,
+  ToastAndroid,
 } from 'react-native';
-
+import axios from 'axios';
+import {connect} from 'react-redux';
+import Spinner from 'react-native-spinkit';
 import {styles} from '../../styles/styleLogin';
+import AsyncStorage from '@react-native-community/async-storage';
 
 export class Login extends Component {
   constructor() {
@@ -24,6 +28,62 @@ export class Login extends Component {
   changeEye = () => {
     const eyes = !this.state.mata;
     this.setState({mata: eyes});
+  };
+
+  userLogin = async () => {
+    this.setState({isloading: true});
+    try {
+      axios
+        .post('https://project-mini.herokuapp.com/api/login', {
+          email: this.state.email,
+          password: this.state.password,
+        })
+        .then((result) => {
+          const {id} = result.data.user;
+          const {kode_member} = result.data.user;
+          const {nama} = result.data.user;
+          const {nomor} = result.data.user;
+          const {saldo} = result.data.user;
+          // const {is_verified} = result.data;
+          const {qrCode} = result.data.user;
+          const {token} = result.data;
+          const {role_id} = result.data.user;
+          console.log('Ini Role==', result.data.user);
+          console.log('Ini Data===', result.data.user);
+          if (
+            token !== null ||
+            role_id !== null ||
+            kode_member !== null ||
+            nama !== null ||
+            id !== null ||
+            nomor !== null ||
+            saldo !== null ||
+            qrCode !== null
+            // is_verified !== null
+          ) {
+            ToastAndroid.show('Anda Berhasil Login', ToastAndroid.LONG);
+            this.setState({
+              isloading: false,
+            });
+          }
+        })
+        .catch((err) => {
+          ToastAndroid.show('Email Atau Password Salah', ToastAndroid.LONG);
+          console.log('Erororo', err);
+          this.setState({
+            isloading: false,
+          });
+        });
+    } catch (err) {
+      this.setState({isloading: false});
+      if (err.response) {
+        ToastAndroid.show(
+          'Maaf Terjadi Kesalahan Dari Kami',
+          ToastAndroid.LONG,
+        );
+        console.log('Erororr ', err.response.data);
+      }
+    }
   };
   render() {
     return (
@@ -49,6 +109,11 @@ export class Login extends Component {
               <TextInput
                 style={styles.input}
                 placeholder="Email Or Phone Number"
+                onChangeText={(email) => {
+                  this.setState({
+                    email: email,
+                  });
+                }}
               />
             </View>
             <Text>Password :</Text>
@@ -61,6 +126,11 @@ export class Login extends Component {
                 secureTextEntry={this.state.mata}
                 style={styles.input}
                 placeholder="Enter Your Password"
+                onChangeText={(password) =>
+                  this.setState({
+                    password: password,
+                  })
+                }
               />
               <TouchableOpacity
                 onPress={() => this.changeEye()}
@@ -80,8 +150,19 @@ export class Login extends Component {
             <TouchableOpacity style={styles.inBottom}>
               <Text style={styles.forgot}>Forgot Password</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.inBottom1}>
-              <Text style={styles.textLogin}>Sign In</Text>
+            <TouchableOpacity
+              onPress={() => this.userLogin()}
+              style={styles.inBottom1}>
+              {this.state.isloading ? (
+                <Spinner
+                  style={styles.loading}
+                  color={'white'}
+                  size={25}
+                  type="Wave"
+                />
+              ) : (
+                <Text style={styles.textLogin}>Sign In</Text>
+              )}
             </TouchableOpacity>
           </View>
           <View style={styles.Register}>
