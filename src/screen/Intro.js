58f9ca3744c486a76d37c41a17927aef2082538d
shin-wolf <amonsefra13@gmail.com>
2021-02-09 14:43:57 +0700

    Create Intro UI

--- src/screen/Intro.js
+++ src/screen/Intro.js
@@ -1,11 +1,45 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, Image, StatusBar, TouchableOpacity} from 'react-native';
+import {styles} from '../styles/styleIntro';
 
 export class Intro extends Component {
   render() {
     return (
-      <View>
-        <Text> textInComponent </Text>
+      <View style={styles.container}>
+        <StatusBar backgroundColor="#F9C900" />
+        <View style={styles.header}>
+          <Text style={styles.textTitle}>Welcome</Text>
+          <Text style={styles.title}>Please login or sign</Text>
+          <Text style={styles.title}>up to continue using our app</Text>
+        </View>
+        <View style={styles.body}>
+          <Image
+            style={styles.icon}
+            source={require('../assets/img/14-removebg-preview.png')}
+          />
+        </View>
+        <View style={styles.pactAccount}>
+          <TouchableOpacity style={styles.login}>
+            <Image
+              style={styles.iconLogin}
+              source={require('../assets/icon/login1.png')}
+            />
+            <Text style={styles.textLogin}>Sign</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.register}>
+            <Image
+              style={styles.iconLogin}
+              source={require('../assets/icon/signUP.png')}
+            />
+            <Text style={styles.textRegister}>Sign Up</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.pactForgot}>
+          <Text>Forgot Password ?</Text>
+          <TouchableOpacity>
+            <Text style={styles.forgot}>Klik</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
---
+++ src/styles/styleIntro.js
@@ -0,0 +1,91 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffff',
+  },
+  header: {
+    height: '15%',
+    // borderWidth: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textTitle: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    marginTop: 25,
+    marginBottom: 10,
+    color: '#F9C900',
+  },
+  title: {
+    fontSize: 15,
+    color: '#a9a9',
+  },
+  icon: {
+    height: '100%',
+    width: '100%',
+  },
+  body: {
+    width: '100%',
+    // borderWidth: 1,
+    height: '50%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 30,
+  },
+  pactAccount: {
+    alignItems: 'center',
+    // borderWidth: 1,
+    height: '20%',
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  iconLogin: {
+    height: 30,
+    width: 30,
+    marginBottom: 5,
+  },
+  login: {
+    height: 85,
+    width: 85,
+    borderWidth: 1,
+    borderColor: '#F9C900',
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight: 25,
+  },
+  textLogin: {
+    color: '#F9C900',
+    fontSize: 14,
+  },
+  register: {
+    height: 85,
+    width: 85,
+    backgroundColor: '#585aa7ff',
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 25,
+  },
+  textRegister: {
+    color: '#ffff',
+  },
+  pactForgot: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '10%',
+    width: '100%',
+    // borderWidth: 1,
+  },
+  forgot: {
+    color: '#F9C900',
+    fontWeight: 'bold',
+    marginLeft: 10,
+    fontSize: 15,
+  },
+});
+
+export {styles};
