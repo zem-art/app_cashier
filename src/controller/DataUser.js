c2ea40b9d44d841e3764bde01a8fd91aa52ec6cf
shin-wolf <amonsefra13@gmail.com>
2021-02-11 10:25:57 +0700

    Create Data Redux

--- src/controller/DataUser.js
+++ src/controller/DataUser.js
@@ -1,14 +1,28 @@
 const defaultstate = {
+  login: false,
   token: '',
   role: '',
+  number: '',
+  image: '',
+  verivic: '',
+  qrcode: '',
+  name: '',
 };
 
 const userData = (state = defaultstate, action) => {
   switch (action.type) {
     case 'SET_USER':
-      return {...state, user: action, payload};
-    case 'SET_ROLE':
-      return {...state, role: action, payload};
+      return {...state, login: true, user: action.payload};
+    case 'SET_NUMBER':
+      return {...state, number: action.payload};
+    case 'SET_IMGAE':
+      return {...state, login: true, image: action.payload};
+    case 'SET_NAME':
+      return {...state, login: true, name: action.payload};
+    case 'SET_QRCODE':
+      return {...state, login: true, qrcode: action.payload};
+    case 'SET_VERIVIC':
+      return {...state, login: true, verivic: action.payload};
     default:
       return state;
   }
