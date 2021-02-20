9d8c2f0a8150154f5d5ef42d99bab61d319dd702
shin-wolf <amonsefra13@gmail.com>
2021-02-20 10:49:31 +0700

    Create Screen Stock Avaible

--- src/screen/Stock.js
+++ src/screen/Stock.js
@@ -1,14 +1,107 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity,
+  Image,
+  StatusBar,
+  ToastAndroid,
+} from 'react-native';
+import {styles} from '../styles/styleStockG';
+import {connect} from 'react-redux';
+import axios from 'axios';
 
 export class Stock extends Component {
+  constructor() {
+    super();
+    this.state = {
+      dataGet: [],
+    };
+  }
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData = async () => {
+    try {
+      await axios
+        .get('https://project-mini.herokuapp.com/api/barang', {
+          headers: {
+            Authorization: `Bearer${this.props.userData.userReducer.token}`,
+          },
+        })
+        .then((result) => {
+          console.log('Sucsees== Get Data', result.data.data);
+          this.setState({dataGet: result.data.data});
+        })
+        .catch((err) => {
+          console.log('Eroro Get Data==', err);
+          ToastAndroid.show('Maaf Terjadi Kesalahan', ToastAndroid.LONG);
+        });
+    } catch (err) {
+      console.log('eroro Get Data', err);
+      ToastAndroid.show('Maaf Terjadi Kesalahan', ToastAndroid.LONG);
+    }
+  };
   render() {
     return (
-      <View>
-        <Text> Stok Gudang </Text>
+      <View style={styles.container}>
+        <StatusBar backgroundColor="#29abe2" />
+        <View style={styles.header}>
+          <TouchableOpacity onPress={() => this.props.navigation.goBack()}>
+            <Image
+              style={styles.back}
+              source={require('../assets/icon/back.png')}
+            />
+          </TouchableOpacity>
+          <Text style={styles.title}>Stok Gudang</Text>
+        </View>
+        <ScrollView>
+          <View style={styles.body}>
+            {this.state.dataGet.map((item) => {
+              return (
+                <View style={styles.inbody}>
+                  <TouchableOpacity style={styles.Pact}>
+                    <Text style={styles.colorDetail}>Detail</Text>
+                  </TouchableOpacity>
+                  <View style={styles.iinBody}>
+                    <View style={styles.ininBody}>
+                      <Text style={styles.colorKet}>Nama Barang</Text>
+                      <Text>{item.nama_barang}</Text>
+                    </View>
+                    <View style={styles.ininBody}>
+                      <Text style={styles.colorKet}>Merek</Text>
+                      <Text>{item.merek}</Text>
+                    </View>
+                  </View>
+
+                  <View style={styles.PactEditDelete}>
+                    <TouchableOpacity style={styles.Pact1}>
+                      <Image
+                        style={styles.delete}
+                        source={require('../assets/icon/delete.png')}
+                      />
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.Pact1}>
+                      <Image
+                        style={styles.delete}
+                        source={require('../assets/icon/edit.png')}
+                      />
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              );
+            })}
+          </View>
+        </ScrollView>
       </View>
     );
   }
 }
-
-export default Stock;
+const mapStateToProps = (state) => {
+  return {
+    userData: state,
+  };
+};
+export default connect(mapStateToProps)(Stock);
---
+++ src/styles/styleStockG.js
@@ -0,0 +1,89 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffff',
+  },
+  header: {
+    // borderWidth: 1,
+    alignItems: 'center',
+    paddingHorizontal: 15,
+    height: 60,
+    flexDirection: 'row',
+    backgroundColor: '#29abe2',
+  },
+  back: {
+    height: 30,
+    width: 30,
+    marginRight: 10,
+  },
+  title: {
+    color: '#ffff',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  body: {
+    // borderWidth: 1,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+  },
+  inbody: {
+    // borderWidth: 1,
+    height: 155,
+    marginVertical: 10,
+    borderRadius: 10,
+    backgroundColor: '#fff',
+    elevation: 9,
+    paddingHorizontal: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  iinBody: {
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    // borderWidth: 1,
+  },
+  ininBody: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    // borderWidth: 1,
+    height: 60,
+    width: '50%',
+  },
+  Pact: {
+    alignSelf: 'flex-end',
+    height: 30,
+    // borderWidth: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 5,
+  },
+  PactEditDelete: {
+    flexDirection: 'row',
+    height: 30,
+    // borderWidth: 1,
+    width: '100%',
+    justifyContent: 'space-between',
+    marginVertical: 5,
+    marginTop: 10,
+  },
+
+  delete: {
+    height: 22,
+    width: 22,
+  },
+  colorDetail: {
+    color: '#0000ff',
+    fontWeight: 'bold',
+    fontSize: 15,
+  },
+  colorKet: {
+    color: '#a9a9',
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+});
+
+export {styles};
