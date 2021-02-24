40797025bc6c9ee9b4424c167b0c25aa8a4ace66
shin-wolf <amonsefra13@gmail.com>
2021-02-24 16:09:13 +0700

    Create Screen Exit

--- src/components/Exit.js
+++ src/components/Exit.js
@@ -1,12 +1,72 @@
 import React, {Component} from 'react';
-import {Text, Image, TouchableOpacity} from 'react-native';
+import {Text, Image, TouchableOpacity, Alert, ToastAndroid} from 'react-native';
 import {styles} from '../styles/styleProfile';
+import {connect} from 'react-redux';
+import AsyncStorage from '@react-native-community/async-storage';
+import axios from 'axios';
 
 export class Exit extends Component {
+  postLogout() {
+    console.log('Mulai Mengirim Permintaan');
+    axios({
+      url: 'https://project-mini.herokuapp.com/api/logout',
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer${this.props.userData.userReducer.token}`,
+        Accept: 'application/json',
+      },
+    })
+      .then((result) => {
+        console.log('Berhasil Keluar==', result.data);
+        ToastAndroid.show('Anda Berhasil Keluar', ToastAndroid.LONG);
+        this.clearData();
+      })
+      .catch((err) => {
+        console.log('Gagal  Data Tidak Terhapus', err);
+      });
+  }
+
+  goTo() {
+    this.props.navigation.navigate('Intro');
+  }
+
+  clearData = async () => {
+    try {
+      console.log('==Sedang Menghapus Data==');
+      const keys = await AsyncStorage.getAllKeys();
+      await AsyncStorage.multiRemove(keys);
+      this.props.userToken(null);
+      this.props.userVerifed(null);
+      this.props.userQrcode(null);
+      this.props.kodeUser(null);
+      this.props.nameUser(null);
+      this.props.numberUser(null);
+      console.log('==Berhasil Menghapus==');
+      this.goTo();
+    } catch (err) {
+      console.log('===Eroro Tidak Bisa Menghapus Data==', err);
+    }
+  };
+
+  Exit = () => {
+    return Alert.alert(
+      'Log Out',
+      'Confirmation',
+      [
+        {
+          text: 'Cancel',
+          onPress: () => ToastAndroid.show('Cancel Log Out', ToastAndroid.LONG),
+          style: 'cancel',
+        },
+        {text: 'Ok ', onPress: () => this.postLogout()},
+      ],
+      {cancelable: false},
+    );
+  };
   render() {
     return (
       <>
-        <TouchableOpacity style={styles.iinBody}>
+        <TouchableOpacity onPress={() => this.Exit()} style={styles.iinBody}>
           <Image
             style={styles.iconCircle}
             source={require('../assets/icon/logOuth.png')}
@@ -17,5 +77,21 @@ export class Exit extends Component {
     );
   }
 }
+const mapStateToProps = (state) => {
+  return {
+    userData: state,
+  };
+};
 
-export default Exit;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    userVerifed: (verifed) => dispatch({type: 'SET_VERIVIC', payload: verifed}),
+    kodeUser: (kode) => dispatch({type: 'SET_KODE', payload: kode}),
+    nameUser: (name) => dispatch({type: 'SET_NAME', payload: name}),
+    numberUser: (data) => dispatch({type: 'SET_NUMBER', payload: data}),
+    userQrcode: (qr) => dispatch({type: 'SET_QRCODE', payload: qr}),
+    userToken: (token) => dispatch({type: 'SET_USER', payload: token}),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Exit);
--- src/components/logOut.js
+++ src/components/logOut.js
@@ -1,30 +1,11 @@
 import React, {Component} from 'react';
 import {Text, TouchableOpacity, Image, Alert, ToastAndroid} from 'react-native';
 import {styles} from '../styles/styleProfilM';
-import axios from 'axios';
+// import axios from 'axios';
 import {connect} from 'react-redux';
 import AsyncStorage from '@react-native-community/async-storage';
 
 export class LogOut extends Component {
-  postLogout() {
-    console.log('Mulai Mengirim Permintaan');
-    axios
-      .post('https://project-mini.herokuapp.com/api/logout', {
-        headers: {
-          Authorazation: `Bearer${this.props.userData.userReducer.token}`,
-        },
-      })
-      .then((result) => {
-        console.log('Berhasil Keluar==', result.data);
-        ToastAndroid.show('Anda Berhasil Keluar', ToastAndroid.LONG);
-        this.clearData();
-        this.goTo();
-      })
-      .catch((err) => {
-        console.log('Gagal  Data Tidak Terhapus', err);
-      });
-  }
-
   goTo() {
     this.props.navigation.navigate('Intro');
   }
