6f618a7a74c49412d42ac4cdd7f07bbfb087009f
shin-wolf <amonsefra13@gmail.com>
2021-02-24 11:46:50 +0700

    Repair Header Up Staf

--- src/components/HeaderStaff.js
+++ src/components/HeaderStaff.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Image, Text, View, TouchableOpacity} from 'react-native';
+import {Text, View} from 'react-native';
 import {styles} from '../styles/styleHomestaf';
 
 export class HomeStaff extends Component {
@@ -7,12 +7,6 @@ export class HomeStaff extends Component {
     return (
       <View style={styles.header}>
         <Text style={styles.title}>Dashboard</Text>
-        <TouchableOpacity>
-          <Image
-            style={styles.iconHeader}
-            source={require('../assets/icon/History.png')}
-          />
-        </TouchableOpacity>
       </View>
     );
   }
